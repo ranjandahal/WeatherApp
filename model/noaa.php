7754bbc5e4a7cf604648f67f5550ef036a1ef31b
qjal <gonekush@gmail.com>
2017-11-28 17:25:52 -0500

    this extracts the current weather. It looks ugly but it can get the current temperature and other fields

--- model/noaa.php
+++ model/noaa.php
@@ -5,4 +5,87 @@
  * Date: 11/23/17
  * Time: 6:24 PM
  */
-require_once('model/constants.php');
+
+
+
+$ch = curl_init('https://api.weather.gov/points/42.3605,-71.0596/forecast/hourly');
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_SSLVERSION => 6,
+    CURLOPT_USERAGENT => 'Firefox',
+]);
+$response = curl_exec($ch);
+if($response === false)
+    exit(curl_error($ch));
+//print_r(json_decode($response));
+
+$response = json_decode($response);
+
+$counter = 1;
+$pcounter = 1;
+$cCounter = 1;
+
+foreach ($response as $item){
+    if($counter == 20)
+        break;
+    if($counter == 4){
+            //print_r($x);
+            foreach ($item as $periods){
+                if($cCounter==5) {
+                    //print_r($periods);
+                    $array = $periods[0];
+                    foreach ($array as $items){
+                        if($pcounter== 6){
+                            $temp =$array->temperature;
+                            $forecast = $array->shortForecast;
+                            $isdaytime= $array->isDaytime;
+                            $wind = $array->windSpeed;
+
+                        }
+                       $pcounter++;
+                    }
+                    break;
+                }
+                $cCounter++;
+            }
+            break;
+    }
+    echo 'Line: ' . $counter;
+    //print_r($item);
+    $counter++;
+    echo "\r\n";
+    //break;
+}
+print_r("this is the temperature ".$temp);
+print_r(" this is the forcast ".$forecast);
+print_r(" this is the isdaytime ".$isdaytime);
+print_r("this is the wind ".$wind);
+echo "<pre>";
+print_r($response);
+echo "<pre>";
+
+//print_r($response);
+
+
+//foreach ($response as $item){
+//    if($counter == 20)
+//        break;
+//    if($counter == 4){
+//        //print_r($x);
+//        foreach ($item as $periods){
+//            if($cCounter==5) {
+//                //print_r($periods);
+//                $array = $periods[0];
+//                foreach ($array as $items){
+//                    if($pcounter== 6){
+//                        print_r($array->temperature);
+//                    }
+//                    $pcounter++;
+//                }
+//                break;
+//            }
+//            $cCounter++;
+//        }
+//        break;
+//    }
