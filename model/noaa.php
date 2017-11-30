4e7f63b7d0585e726983978d3dcb63d4f4623ff2
qjal <gonekush@gmail.com>
2017-11-29 22:26:13 -0500

    Modified the function calls, to get the data fields, for temp, wind, isday status. can return data for any number of required hours ranging from 1, 2, 3, 6, 12. Some clean up still necessary

--- model/noaa.php
+++ model/noaa.php
@@ -1,91 +1,124 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Ranjan Dahal
+ * User: Kushal Khanal
  * Date: 11/23/17
  * Time: 6:24 PM
+
+ * Written By kushal Khanal
+ * Uses google Api call from Bikesh Manandhar's Darksky, to get the lat, and lon
+ * the data response is not standard thus, has multiple for loops to extract the
+ * required field.
+ * $size is required to know either weather data for 1 hour, 6 hour, or 12 hr is required
+ * the raw data gives weather data for more than next 50 hrs. thus, required hours can be extracted
+ * Still uncomplete for Ip implementation
+
  */
 
 
 
-$ch = curl_init('https://api.weather.gov/points/42.3605,-71.0596/forecast/hourly');
-curl_setopt_array($ch, [
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_SSLVERSION => 6,
-    CURLOPT_USERAGENT => 'Firefox',
-]);
-$response = curl_exec($ch);
-if($response === false)
-    exit(curl_error($ch));
-//print_r(json_decode($response));
+function getvalues($response, $size){
 
-$response = json_decode($response);
+    $counter = 1;
+    $pcounter = 1;
+    $cCounter = 1;
+    $index = 0;
+    $arr = array($size);
 
-$counter = 1;
-$pcounter = 1;
-$cCounter = 1;
-
-foreach ($response as $item){
-    if($counter == 20)
-        break;
-    if($counter == 4){
+    foreach ($response as $item){
+        if($counter == 20)
+            break;
+        if($counter == 4){
             //print_r($x);
             foreach ($item as $periods){
                 if($cCounter==5) {
-                    //print_r($periods);
-                    $array = $periods[0];
-                    foreach ($array as $items){
-                        if($pcounter== 6){
-                            $temp =$array->temperature;
-                            $forecast = $array->shortForecast;
-                            $isdaytime= $array->isDaytime;
-                            $wind = $array->windSpeed;
+                    foreach ($periods as $vals) {
+                        $array =$periods[$index];
+                        foreach ($array as $items){
+                            if($pcounter== 6){
+                                $temp =$array->temperature;
+                                $forecast = $array->shortForecast;
+                                $isdaytime= $array->isDaytime;
+                                $wind = $array->windSpeed;
+                                $hour_object = array('temp' => $temp,
+                                    'forecast' => $forecast,
+                                    'isdaytime' => $isdaytime,
+                                    'wind' => $wind,
+                                );
+                                print_r($hour_object['temp']);
+                                echo "\r\n";
+                                $arr[$index]= $hour_object;
 
+                                break;
+                            }
+                            $pcounter++;
                         }
-                       $pcounter++;
+                        $pcounter= 0;
+                        $index++;
+                        if($index==6) {
+                            break;
+                        }
                     }
-                    break;
                 }
                 $cCounter++;
             }
             break;
+        }
+        echo 'Line: ' . $counter;
+        $counter++;
+        echo "\r\n";
     }
-    echo 'Line: ' . $counter;
-    //print_r($item);
-    $counter++;
-    echo "\r\n";
-    //break;
+    return $arr;
 }
-print_r("this is the temperature ".$temp);
-print_r(" this is the forcast ".$forecast);
-print_r(" this is the isdaytime ".$isdaytime);
-print_r("this is the wind ".$wind);
-echo "<pre>";
-print_r($response);
-echo "<pre>";
 
-//print_r($response);
+function get_address_info($searchedCity){
+    // GOOGLE API KEY for getting geolocation info
+    $GOOGLE_API_KEY_s1 = "AIzaSyBhBuNbrzlIhOJjiqRTAYWPlx9x_sp4QFM";
+
+    //Allowing_url_fopen to be enabled so that we can use file_get_contents.
+    ini_set("allow_url_fopen",1);
+    $url_link = "https://maps.googleapis.com/maps/api/geocode/json?address=".rawurlencode("{$searchedCity}")."&key={$GOOGLE_API_KEY_s1}";
+    $json_url = file_get_contents($url_link);
+    $json_obj = json_decode($json_url,true);
+
+    if($json_obj['status'] == "OK")
+        return $json_obj['results'][0]['geometry']['location'];
+    else
+        return "ERROR";
+}
+
+function get_raw_data_noaa($loc_info_noaa){
+
+    //Latitude and Longitude
+    $latitude = $loc_info_noaa['lat'];
+    $longitude = $loc_info_noaa['lng'];
 
 
-//foreach ($response as $item){
-//    if($counter == 20)
-//        break;
-//    if($counter == 4){
-//        //print_r($x);
-//        foreach ($item as $periods){
-//            if($cCounter==5) {
-//                //print_r($periods);
-//                $array = $periods[0];
-//                foreach ($array as $items){
-//                    if($pcounter== 6){
-//                        print_r($array->temperature);
-//                    }
-//                    $pcounter++;
-//                }
-//                break;
-//            }
-//            $cCounter++;
-//        }
-//        break;
-//    }
+    //Allowing_url_fopen to be enabled so that we can use file_get_contents.
+    $ch = curl_init('https://api.weather.gov/points/'.$latitude.','.$longitude.'/forecast/hourly');
+    curl_setopt_array($ch, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_SSLVERSION => 6,
+        CURLOPT_USERAGENT => 'Firefox',
+    ]);
+    $response = curl_exec($ch);
+    if($response === false)
+        exit(curl_error($ch));
+
+    $response = json_decode($response);
+    return $response;
+
+
+}
+
+
+function noaa_forecast($searchedinput, $size){
+
+    $location_info = get_address_info($searchedinput);
+    $raw_data =  get_raw_data_noaa($location_info);
+    $values = getvalues($raw_data, $size);
+    return $values;
+}
+
+?>
