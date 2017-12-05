29322f2f1c4c683bcf655f3de57253167570da90
qjal <gonekush@gmail.com>
2017-12-05 15:31:09 -0500

    this extracts the current weather. It looks ugly but it can get the current temperature and other fields

--- model/noaa.php
+++ model/noaa.php
@@ -12,12 +12,16 @@
  * $size is required to know either weather data for 1 hour, 6 hour, or 12 hr is required
  * the raw data gives weather data for more than next 50 hrs. thus, required hours can be extracted
  * Still uncomplete for Ip implementation
-
+w
  */
 
 
+require_once('accuweather.php');
+noaa_forecast('158.121.249.23', '6');
+noaa_forecast('02149', '1');
+noaa_forecast('everett, MA', 2);
+function get_values($response, $size){
 
-function getvalues($response, $size){
 
     $counter = 1;
     $pcounter = 1;
@@ -26,7 +30,7 @@ function getvalues($response, $size){
     $arr = array($size);
 
     foreach ($response as $item){
-        if($counter == 20)
+        if($counter == 6)
             break;
         if($counter == 4){
             //print_r($x);
@@ -37,15 +41,28 @@ function getvalues($response, $size){
                         foreach ($array as $items){
                             if($pcounter== 6){
                                 $temp =$array->temperature;
-                                $forecast = $array->shortForecast;
-                                $isdaytime= $array->isDaytime;
-                                $wind = $array->windSpeed;
-                                $hour_object = array('temp' => $temp,
-                                    'forecast' => $forecast,
-                                    'isdaytime' => $isdaytime,
-                                    'wind' => $wind,
+                                $icon_phrase = $array->shortForecast;
+                                $time = $array->startTime;
+                                $time = explode('T',$time);
+                                $time = substr($time[1],0,5);
+                                $icon =$array->icon;
+                                $wind_speed = $array->windSpeed;
+
+                                $hour_object = array(
+                                    'hour'=> $time,
+                                    'icon'=> $icon,
+                                    'icon_phrase' => $icon_phrase,
+                                    'temp' => $temp,
+                                    'real_feel_temp'=> null,
+                                    'wind_speed'=> $wind_speed,
+                                    'humidity'=> null,
+                                    'rain_probability' => null,
+                                    'snow_probability' => null,
+                                    'cloud_cover' => null,
+                                    'prep' => null,
+
                                 );
-                                print_r($hour_object['temp']);
+                                //print_r($hour_object['temp']);
                                 echo "\r\n";
                                 $arr[$index]= $hour_object;
 
@@ -64,7 +81,7 @@ function getvalues($response, $size){
             }
             break;
         }
-        echo 'Line: ' . $counter;
+       // echo 'Line: ' . $counter;
         $counter++;
         echo "\r\n";
     }
@@ -75,16 +92,24 @@ function get_address_info($searchedCity){
     // GOOGLE API KEY for getting geolocation info
     $GOOGLE_API_KEY_s1 = "AIzaSyBhBuNbrzlIhOJjiqRTAYWPlx9x_sp4QFM";
 
-    //Allowing_url_fopen to be enabled so that we can use file_get_contents.
-    ini_set("allow_url_fopen",1);
-    $url_link = "https://maps.googleapis.com/maps/api/geocode/json?address=".rawurlencode("{$searchedCity}")."&key={$GOOGLE_API_KEY_s1}";
-    $json_url = file_get_contents($url_link);
-    $json_obj = json_decode($json_url,true);
+    if (filter_var($searchedCity, FILTER_VALIDATE_IP)) {
+        //echo("$searchedCity is a valid IP address");
+        $datas = get_current_location_id($searchedCity);
+        return $datas['geo'];
 
-    if($json_obj['status'] == "OK")
-        return $json_obj['results'][0]['geometry']['location'];
-    else
-        return "ERROR";
+    } else {
+
+        //Allowing_url_fopen to be enabled so that we can use file_get_contents.
+        ini_set("allow_url_fopen", 1);
+        $url_link = "https://maps.googleapis.com/maps/api/geocode/json?address=" . rawurlencode("{$searchedCity}") . "&key={$GOOGLE_API_KEY_s1}";
+        $json_url = file_get_contents($url_link);
+        $json_obj = json_decode($json_url, true);
+
+        if ($json_obj['status'] == "OK") {
+            return $json_obj['results'][0]['geometry']['location'];
+        }else
+            return "ERROR";
+    }
 }
 
 function get_raw_data_noaa($loc_info_noaa){
@@ -117,8 +142,9 @@ function noaa_forecast($searchedinput, $size){
 
     $location_info = get_address_info($searchedinput);
     $raw_data =  get_raw_data_noaa($location_info);
-    $values = getvalues($raw_data, $size);
+    $values = get_values($raw_data, $size);
+    echo  $values[0]['temp'];
+    echo $values[0]['hour'];
     return $values;
 }
-
 ?>
